5df278eeb048afc0da4b44d02882e5c8734f9757
jan Ne <46408244+jan-Ne@users.noreply.github.com>
2024-07-10 21:09:19 -0400

    Create readMenu.js

---
+++ js/readMenu.js
@@ -0,0 +1,27 @@
+// Font Button Click
+  function fontClick() {
+    $(".font-menu").toggle();
+  }
+  
+// Size Functions
+  var scale = 1;
+  function increaseSize() {
+    if (scale < 4.9){
+      scale += 0.1;
+    }
+    $(".content").css("transform", "scale(" + scale + ")");
+    $(".content-scroll").css("width", "calc(816px \* " + scale + ")");
+    if (scale >= 1){
+      $(".content-scroll").css("height", "calc(" + $(".content").height() + "px \* " + scale + " \+ 17px");
+    }
+  }
+  function decreaseSize() {
+    if (scale > 0.6){
+      scale -= 0.1;
+    }
+    $(".content").css("transform", "scale(" + scale + ")");
+    $(".content-scroll").css("width", "calc(816px \* " + scale + ")");
+    if (scale > 1){
+      $(".content-scroll").css("height", "calc(" + $(".content").height() + "px \* " + scale + " \+ 17px");
+    }
+  }
